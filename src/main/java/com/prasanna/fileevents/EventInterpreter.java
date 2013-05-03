153647ec5e8cb56e1be8af219896ac231bddcadc
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-05-03 11:54:29 -0700

    why the fuck was  I using '== false' instead of !

--- src/main/java/com/prasanna/fileevents/EventInterpreter.java
+++ src/main/java/com/prasanna/fileevents/EventInterpreter.java
@@ -103,7 +103,7 @@ public class EventInterpreter
 
     private void interpretAddEvent(Event event)
     {
-        if (eventStack.isEmpty() == false)
+        if (!eventStack.isEmpty())
         {
             if (isAPossibleMoveOrRename(event))
             {
